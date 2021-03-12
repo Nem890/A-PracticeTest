ebe38e23b0913fca18ed60f265c1a6345c1a101c
Michael Alestock <75401074+elmatic@users.noreply.github.com>
2021-03-12 06:51:46 -0500

    Updated CompTIA A+ Core 1 Practice Test.py
    Removed the importation of 'practiceLibrary'.

--- CompTIA A+ Core 1 Practice Test.py
+++ CompTIA A+ Core 1 Practice Test.py
@@ -1,8 +1,5 @@
 # Michael Alestock
 
-# Import Practice Test Library
-
-import practiceLibrary
 
 # Display Instructions to the Student
 def show_instructions():
@@ -10,14 +7,15 @@ def show_instructions():
     print("You have 90 minutes to finish the test\n")
     print("Good luck!!\n")
 
+
 show_instructions()  # Calling the function
 
 # Declare Test Questions & Answers
 
-practiceLibrary.question1 = ["110", "143", "443", "21"]
+question1 = ["110", "143", "443", "21"]
 attempts = list(range(2))
 
-while (1):
+while 1:
     print("What port does FTP use?\n110\n143\n443\n21 ")
     answer = input("Answer: ")
     if answer == "21":
